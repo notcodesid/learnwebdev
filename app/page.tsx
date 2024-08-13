18a720709d49042d685e865852a4c3d5b50ca44d
siddharth <notcodesid@gmail.com>
2024-08-13 20:42:34 +0530

    herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Appbar from "@/components/appbar";
+import HeroSection from "@/components/heroScetion";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <Appbar />
+    <HeroSection />
     </>
 
   );
---
+++ components/heroScetion.tsx
@@ -0,0 +1,29 @@
+
+
+export default function HeroSection() {
+    return (
+      <div className="max-w-3xl mx-auto px-4 py-8 md:py-12">
+        <p className="text-center text-orange-500 font-semibold mb-4">100% Free • 12-Week Bootcamp</p>
+        <h1 className="text-4xl md:text-5xl font-bold text-center mb-6">
+        Zero to Full-Stack Hero:
+        Web Dev Mastery Course<br />for Beginners
+        </h1>
+        <div className="space-y-6 text-gray-600">
+          <p className="text-center">
+          Transform your career without breaking the bank. No CS degree required.
+          </p>
+          <div>
+            <h2 className="font-semibold text-black mb-2">Why is this free?</h2>
+            <p>Because it makes no sense to sell courses that teach basics.</p>
+          </div>
+          <div>
+            <p>
+              First earn ₹30K per month and pay your bills. Don't worry. Industry sell a lot of expensive stuff too, you can
+              buy them once you have the basic knowledge.
+            </p>
+          </div>
+          <p className="font-semibold text-black">But first, learn the basics for free.</p>
+        </div>
+      </div>
+    )
+  }
