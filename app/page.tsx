6575da223d9199b9f7a65e93a2aa60728301771d
siddharth <notcodesid@gmail.com>
2024-08-14 15:17:06 +0530

    week 6 git & github

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Week2 from "@/components/week/week2";
 import Week3 from "@/components/week/week3";
 import Week4 from "@/components/week/week4";
 import Week5 from "@/components/week/week5";
+import Week6 from "@/components/week/week6";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
     <Week3 />
     <Week4 />
     <Week5 />
+    <Week6 />
   
     </>
 
