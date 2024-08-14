c83db7682d0fdac59163c95dc09389015f29644d
siddharth <notcodesid@gmail.com>
2024-08-14 13:32:16 +0530

    week 5 html,css & js projects

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Week1 from "@/components/week/week1";
 import Week2 from "@/components/week/week2";
 import Week3 from "@/components/week/week3";
 import Week4 from "@/components/week/week4";
+import Week5 from "@/components/week/week5";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
     <Week2 />
     <Week3 />
     <Week4 />
+    <Week5 />
   
     </>
 
