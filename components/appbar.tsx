0e058925570b51fd4f623259d19e13ad43b113d6
siddharth <notcodesid@gmail.com>
2025-01-08 20:04:30 +0530

    add dockerfile

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -1,32 +1,25 @@
-import Image from "next/image"
-import profie from "@/public/x-pfp.png"
-import Link from "next/link"
-import {
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton
-  } from '@clerk/nextjs'
+import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Appbar() {
-    return (
-        <header className="sticky top-0 bg-white flex h-16 items-center justify-end border-b px-4 lg:px-6">
-            <Link href="https://project-ideas-nine.vercel.app/">
-            <p className="mr-10">Project Ideas</p>
-            </Link>
-            <div className="flex items-center gap-4">
-                <span className="text-sm text-muted-foreground">
-                Joined with heart by
-                </span>
-                 <span>
-                 <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-                 </span>
-            </div>
-        </header>
-    )
+  return (
+    <header className="sticky top-0 bg-white flex h-16 items-center justify-end border-b px-4 lg:px-6">
+      <Link href="https://project-ideas-nine.vercel.app/">
+        <p className="mr-10">Project Ideas</p>
+      </Link>
+      <div className="flex items-center gap-4">
+        <span className="text-sm text-muted-foreground">
+          Joined with heart by
+        </span>
+        <span>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </span>
+      </div>
+    </header>
+  );
 }
