77554531e1bb7d463ea56abc33e45da4c9deea12
notcodesid <notcodesid@gmail.com>
2025-04-26 16:37:27 +0530

    added open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "learnwebdev",
-  description: "a platform where user can learn web dev for free.",
+  description: "learn web development for free",
+  metadataBase: new URL("https://learnwebdev.notcodesid.xyz/"),
+  openGraph: {
+    title: "learn web development for free",
+    description: "learn web development for free",
+    url: "https://learnwebdev.notcodesid.xyz/",
+    siteName: "learnwebdev",
+    images: [
+      {
+        url: "/openimage.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-IN",
+    type: "article",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "learnwebdev",
+    description: "learn web development for free",
+    images: ["/openimage.png"],
+  },
 };
 
 export default function RootLayout({
@@ -19,8 +41,8 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className={inter.className}>
-
         {children}
+        <Analytics />
       </body>
     </html>
   );
